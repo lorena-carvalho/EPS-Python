63dd496c921fecfe143dc3164aa503c3a9420bec
Lorena Carvalho <78042293+lorena-carvalho@users.noreply.github.com>
2022-05-27 15:33:02 +0000

    Create eps

---
+++ EstruturaDeDecisão/ep01.py
@@ -0,0 +1,10 @@
+num1 = int(input("Informe um número: "))
+num2 = int(input("Informe outro número: "))
+
+if (num1 > num2):
+  print(num1,"é maior que", num2)
+elif (num1 < num2):
+  print(num2,"é maior que", num1)
+else:
+  print("Os números são iguais")
+  
