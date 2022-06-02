eca27e4ce039ffb1a668eb5cf0727d0af45a878b
Lorena Carvalho <78042293+lorena-carvalho@users.noreply.github.com>
2022-06-02 12:06:44 +0000

    Create eps

--- EstruturaDeDecisão/ep07.py
+++ EstruturaDeDecisão/ep07.py
@@ -8,3 +8,10 @@ elif(b < a and b < c):
   print("Menor número:",b)
 elif(c < a and c < b):
   print("Menor número:",c)
+
+if (a > b and a > c):
+  print("Maior número:",a)
+elif(b > a and b > c):
+  print("Maior número:",b)
+elif(c > a and c > b):
+  print("Maior número:",c)
